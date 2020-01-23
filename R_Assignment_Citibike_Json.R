b568b974ba74d72c3f548aea587d3f64762dc538
Frank1025 <59177289+Frank1025@users.noreply.github.com>
2020-01-23 11:55:26 +0000

    Add files via upload

---
+++ R_Assignment_Citibike_Json.R
@@ -0,0 +1,11 @@
+browseURL("http://citibikenyc.com/stations/json")
+city<-fromJSON("http://citibikenyc.com/stations/json")
+str(city)
+stations<-city$stationBeanList
+colnames(stations)
+is.data.frame(stations)
+stationName<-city$stationBeanList$stationName
+stationName
+is.atomic(stationName)
+plot(id)
+# It seems to me that there's a limit to the amount of output the console can display.Please is that the case? @ Edison.
